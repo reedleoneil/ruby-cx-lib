b802a59bd86f7cb8b1061e1f3a0722370c46afc2
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-02 18:48:53 +0900

    Create instrument class.

---
+++ lib/cx/instrument.rb
@@ -0,0 +1,12 @@
+class Instrument
+  attr_accessor :InstrumentId, :Symbol, :Product1, :Product1Symbol, :Product2, :Product2Symbol, :InstrumentType
+  def initialize(instrument_id, symbol, product_1, product_1_symbol, product_2, product_2_symbol, instrument_type)
+    @InstrumentId = instrument_id
+    @Symbol = symbol
+    @Product1 = product_1
+    @Product1Symbol = product_1_symbol
+    @Product2 = product_2
+    @Product2Symbol = product_2_symbol
+    @InstrumentType = instrument_type
+  end
+end
