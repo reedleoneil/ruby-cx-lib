cfc21f2a83a337ee4065f9e3c749878712e52118
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-02 18:46:05 +0900

    Create frame class.

---
+++ lib/cx/frame.rb
@@ -0,0 +1,9 @@
+class Frame
+  attr_accessor :message_type, :sequence_number, :function_name, :payload
+  def initialize(m, i, n, o)
+    @message_type = m
+    @sequence_number = i
+    @function_name = n
+    @payload = o
+  end
+end
