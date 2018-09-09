70d4320f4c6b68af2813056b2b5761accfdef95c
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-09 18:57:03 +0900

    Update frame class.

--- lib/cx/frame.rb
+++ lib/cx/frame.rb
@@ -1,11 +1,5 @@
 class Frame
   attr_accessor :message_type, :sequence_number, :function_name, :payload
-  def initialize(m, i, n, o)
-    @message_type = m
-    @sequence_number = i
-    @function_name = n
-    @payload = o
-  end
 
   def self.serialize(frame)
     return {'m' => frame.message_type, 'i' => frame.sequence_number, 'n' => frame.function_name, 'o' => frame.payload.to_json}.to_json
@@ -13,7 +7,11 @@ class Frame
 
   def self.deserialize(json)
     hash = JSON.parse(json)
-    frame = Frame.new(hash["m"], hash["i"], hash["n"], hash["o"])
+    frame = Frame.new
+    frame.message_type = hash["m"]
+    frame.sequence_number = hash["i"]
+    frame.function_name = hash["n"]
+    frame.payload = hash["o"]
     frame.payload = JSON.parse(frame.payload)
     return frame
   end
