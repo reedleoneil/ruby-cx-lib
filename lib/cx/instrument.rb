ce9508df76bc2b1a4cc3cc4aad29af3396570858
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-09 18:13:24 +0900

    Update instrument class.

--- lib/cx/instrument.rb
+++ lib/cx/instrument.rb
@@ -1,12 +1,12 @@
 class Instrument
-  attr_reader :InstrumentId, :Symbol, :Product1, :Product1Symbol, :Product2, :Product2Symbol, :InstrumentType
+  attr_reader :intrument_id, :symbol, :product_1, :product_1_symbol, :product_2, :product_2_symbol, :instrument_type
   def initialize(instrument_id, symbol, product_1, product_1_symbol, product_2, product_2_symbol, instrument_type)
-    @InstrumentId = instrument_id
-    @Symbol = symbol
-    @Product1 = product_1
-    @Product1Symbol = product_1_symbol
-    @Product2 = product_2
-    @Product2Symbol = product_2_symbol
-    @InstrumentType = instrument_type
+    @instrument_id = instrument_id
+    @symbol = symbol
+    @product_1 = product_1
+    @product_1_symbol = product_1_symbol
+    @product_2 = product_2
+    @product_2_symbol = product_2_symbol
+    @instrument_type = instrument_type
   end
 end
