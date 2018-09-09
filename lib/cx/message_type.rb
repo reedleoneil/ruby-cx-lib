b118faa3ee9067f3a5881857c2b5cd9e0dd45b95
reedleoneil <reedleoneilpascual@yahoo.com>
2018-09-09 23:02:55 +0900

    Changed response to reply.

--- lib/cx/message_type.rb
+++ lib/cx/message_type.rb
@@ -1,6 +1,6 @@
 class MessageType
   def self.request; 0 end
-  def self.response; 1 end
+  def self.reply; 1 end
   def self.subscribe; 2 end
   def self.event; 3 end
   def self.unsubscribe; 4 end
